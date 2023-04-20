efd2f55c32cf587b4696c7d80bbf68d9a34b6d32
Agustin1486 <atunez30@gmail.com>
2023-04-20 20:22:27 -0300

    update

--- server/src/controllers/getCharById.js
+++ server/src/controllers/getCharById.js
@@ -1,20 +1,47 @@
-const axios = require ("axios")
+//Con Express
+const axios = require("axios");
+const URL = "https://rickandmortyapi.com/api/character/";
 
-
-function getCharById (res,id){
-axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-.then((response)=>{
-    const{id,name,gender,species,origin,image,status} = response.data;
-res.writeHead(200,{"Content-Type": "application.json"});
-res.end(
-    JSON.stringify({id,name,gender,species,origin,image,status})); 
-})
-
-.catch((error)=>{
-    res.writeHead(500,{"Content-Type":"text/plain"})
-    res.end(error.message);
-})
-
+const getCharById = (req, res) => {
+  const { id } = req.params;
+  axios
+    .get(URL + id)
+    .then((response) => {
+      const character = response.data;
+      if (character.error) {
+        res.status(404).json({ message: "Not Found" });
+      } else {
+        const { id, status, name, species, origin, image, gender } = character;
+        res.json({ id, status, name, species, origin, image, gender });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    });
 }
 
+
 module.exports = getCharById;
+
+//Sin Express
+/* const axios = require("axios");
+const URL = "https://rickandmortyapi.com/api/character/";
+const getCharById = (res, id) => {
+  axios
+    .get(URL + id)
+    .then((response) => {
+        const {id, name, gender, species, origin, image, status} = response.data
+        res.writeHead(200, {"Content-Type":"application/json"})
+        // { id:1 } formato --> obj de JS
+        // { "id": 1} formato --> JSON
+        res.end(
+          JSON.stringify({ id, name, gender, species, origin, image, status })
+        );
+    })
+    .catch((reason) => {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(reason.message)
+    });
+};
+module.exports = getCharById; */
--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,50 @@
-const http = require ("http")
-const PORT = 3001
-const getCharById = require ("./controllers/getCharById")
+//Usando (Express)
+const express = require("express");
+const server = express();
+const PORT = 3001;
+const router = require("./routes/index");
+//middleware .use y sus acciones(header)
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+
+  next();
+});
 
 
-http.createServer((req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    const {url} = req
-        
-        //aqui esta mi RUTA    
-       if(url.includes("/rickandmorty/character")){
-        
-        const characterId = url.split("/").at(-1)
-        /* const character = data.filter((e)=>e.id === Number(characterId)); */
-        //AQUI ESTA MI CONTROLLER
-        getCharById(res,characterId)
-   
-    
-    }else{
-        res.writeHead(404,{"Content-Type": "text/plain"})
-        res.end("Ocurrio un error");
+server.use(express.json());//middleware
+server.use("/rickandmorty", router);//middleware
+
+server.listen(PORT, () => {
+  console.log("Server raised in port: " + PORT);
+});
+
+//---------------------------------------------------------------------------
+// usando (HTTP) con createServer
+/* const http = require("http");
+const getCharById = require("./controllers/getCharById");
+const PORT = 3001;
+http
+  .createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    console.log(req.url);
+    try {
+      const { url } = req;
+      if (url.includes("/rickandmorty/character")) {
+        const id = url.split("/").at(-1);
+        console.log(id)
+        getCharById(res, id);
+      }
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
     }
-
-
-
-}).listen(PORT, "localhost");
-
+  })
+  .listen(PORT, () => {
+    console.log(`Server on port ${PORT}`);
+  }); */
